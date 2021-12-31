687f36be94f4af489dd92ec986d979138f7023ac
ZY <1399664760@qq.com>
2022-01-01 00:19:17 +0800

    2022/01/01

--- com_boot/com_basic/src/main/java/com/basic/controller/BasicController.java
+++ com_boot/com_basic/src/main/java/com/basic/controller/BasicController.java
@@ -2,6 +2,7 @@ package com.basic.controller;
 
 import com.basic.service.BasicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +18,12 @@ public class BasicController {
     @Autowired
     private BasicService basicService;
 
+    @Value( "${server.port}" )
+    String port;
     @PostMapping("/query")
     public Map<String,Object> query(@RequestBody Map<String,Object> map ){
         HashMap map1 = new HashMap();
-        map1.put("tuo","你好");
+        map1.put("tuo",port);
         return map1;
     }
 }
--- com_boot/com_basic/src/main/java/com/basic/service/impl/BasicServiceImpl.java
+++ com_boot/com_basic/src/main/java/com/basic/service/impl/BasicServiceImpl.java
@@ -1,14 +1,22 @@
 package com.basic.service.impl;
 
 import com.basic.service.BasicService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
 public class BasicServiceImpl implements BasicService {
+    private static Logger logger = LoggerFactory.getLogger( BasicServiceImpl.class );
     @Override
     public Map<String, Object> query() {
+        try{
+
+        }catch (Exception e){
+           throw e;
+        }
         return null;
     }
 }
